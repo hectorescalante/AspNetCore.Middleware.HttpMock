be9876c97c40976cd8b741af63d58c6b2a0ee696
Hector Escalante <hector.samuel.escalante@outlook.com>
2020-06-06 12:16:25 +0200

    safe string conversion

--- src/AspNetCore.Middleware.HttpMock/AspNetCore.Middleware.HttpMock/Core/StringExtensions.cs
+++ src/AspNetCore.Middleware.HttpMock/AspNetCore.Middleware.HttpMock/Core/StringExtensions.cs
@@ -6,17 +6,35 @@ namespace AspNetCore.Middleware.HttpMock.Core
 {
   public static class StringExtensions
   {
-    private readonly static JsonSerializerOptions _jsonSerializerOptions = 
+    private readonly static JsonSerializerOptions _jsonSerializerOptions =
       new JsonSerializerOptions() { PropertyNameCaseInsensitive = true, IgnoreNullValues = true };
 
     public static string ToJsonString<T>(this T entity) =>
       JsonSerializer.Serialize(entity, _jsonSerializerOptions);
 
-    public static T FromJsonString<T>(this string jsonString) =>
-      JsonSerializer.Deserialize<T>(jsonString, _jsonSerializerOptions);
+    public static T FromJsonString<T>(this string jsonString)
+    {
+      try
+      {
+        return JsonSerializer.Deserialize<T>(jsonString, _jsonSerializerOptions);
+      }
+      catch (Exception)
+      {
+        return default;
+      }
+    }
 
-    public static string FromBase64String(this string base64String) =>
-      Encoding.UTF8.GetString(Convert.FromBase64String(base64String ?? ""));
+    public static string FromBase64String(this string base64String)
+    {
+      try
+      {
+      return Encoding.UTF8.GetString(Convert.FromBase64String(base64String ?? ""));
+      }
+      catch (Exception)
+      {
+        return default;
+      }
+    }
 
     public static string ToBase64String(this string plainText) =>
       Convert.ToBase64String(Encoding.UTF8.GetBytes(plainText ?? ""));
